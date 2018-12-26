7ea0c325a9b4da62232059123010c85028dcd7ab
skull <skull@darktemple.ch>
2018-12-26 18:53:15 +0100

    still optionsview

--- src/main/java/ch/zhaw/roma/controller/OptionsViewController.java
+++ src/main/java/ch/zhaw/roma/controller/OptionsViewController.java
@@ -1,4 +1,31 @@
 package ch.zhaw.roma.controller;
 
-public class OptionsViewController {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class OptionsViewController implements Initializable {
+    //region FXML fields
+    public Button cancelButton;
+    public Button loadButton;
+    public Button saveButton;
+    //endregion
+
+    //region Event Handlers
+    public void onCancelAction(ActionEvent actionEvent) {
+    }
+
+    public void onLoadAction(ActionEvent actionEvent) {
+    }
+
+    public void onSaveAction(ActionEvent actionEvent) {
+    }
+    //endregion
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
