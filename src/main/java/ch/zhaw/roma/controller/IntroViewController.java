6d4ac654bb1755eb1c922252149991696b5eba3f
skull <skull@darktemple.ch>
2019-01-05 17:44:27 +0100

    save

--- src/main/java/ch/zhaw/roma/controller/IntroViewController.java
+++ src/main/java/ch/zhaw/roma/controller/IntroViewController.java
@@ -1,13 +1,33 @@
 package ch.zhaw.roma.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class IntroViewController implements Initializable {
+    public Button editData;
+    public Button startNewRoyaltyCalculation;
+    public Button openSettings;
+    public Button importExcel;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void openNewCalculation(ActionEvent actionEvent) {
+    }
+
+    public void openExcelFileImporter(ActionEvent actionEvent) {
+    }
+
+    public void openDataEditor(ActionEvent actionEvent) {
+    }
+
+    public void openSettings(ActionEvent actionEvent) {
     }
 }
