1f16b73ce0b159853a0ffd7a0a9ea912fd1696ac
skull <skull@darktemple.ch>
2019-01-05 14:34:42 +0100

    RoyaltyCalculation skizze gemachdt

---
+++ src/main/java/ch/zhaw/roma/controller/IntroViewController.java
@@ -0,0 +1,13 @@
+package ch.zhaw.roma.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class IntroViewController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
