dde3032d18c70215364d84241ff4073bca105acb
skull <skull@darktemple.ch>
2019-01-06 18:32:18 +0100

    save

--- src/main/java/ch/zhaw/roma/controller/IntroViewController.java
+++ src/main/java/ch/zhaw/roma/controller/IntroViewController.java
@@ -1,10 +1,15 @@
 package ch.zhaw.roma.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,10 +18,21 @@ public class IntroViewController implements Initializable {
     public Button startNewRoyaltyCalculation;
     public Button openSettings;
     public Button importExcel;
+    private Stage settingsViewStage = new Stage();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        settingsViewStage.setTitle("Einstellungen...");
+        try {
+            settingsViewStage.setScene(
+                new Scene(
+                    FXMLLoader.load(getClass().getResource("/view/SettingsView.fxml"))
+                )
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void openNewCalculation(ActionEvent actionEvent) {
@@ -29,5 +45,6 @@ public class IntroViewController implements Initializable {
     }
 
     public void openSettings(ActionEvent actionEvent) {
+        settingsViewStage.show();
     }
 }
