b4063adff10a9d4b1d2ac81ce2016cc640c481eb
skull <skull@darktemple.ch>
2019-01-19 09:52:45 +0100

    todoliste erstellt

--- src/main/java/ch/zhaw/roma/Main.java
+++ src/main/java/ch/zhaw/roma/Main.java
@@ -4,23 +4,21 @@ import ch.zhaw.roma.interfaces.IRepositority;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 import javafx.scene.layout.BorderPane;
-//import javax.persistence.*;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class Main extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
-    private IRepositority repositority;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         try {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+            loader.setLocation(Main.class.getResource("view/IntroView.fxml.fxml"));
             rootLayout = (BorderPane) loader.load();
             this.primaryStage = primaryStage;
             this.primaryStage.setTitle("RoHo - Royalty Management System");
@@ -29,27 +27,10 @@ public class Main extends Application {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        showEditRoMaNote();
-    }
-
-
-    public void showEditRoMaNote() {
-
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("view/EditRoMaNote.fxml"));
-            AnchorPane editRoMaNote = (AnchorPane) loader.load();
-            rootLayout.setCenter(editRoMaNote);
-        } catch (IOException e) {
-                e.printStackTrace();
-        }
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
-
-    // EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ch.zhaw.roma.jpa");
 }
