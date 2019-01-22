23ec253b03b34bc78e26c2fa735f25b76c7a73cb
red-rackham <git@jpost.ch>
2019-01-22 20:42:11 +0100

    jUnit Test

--- src/test/java/ch/zhaw/roma/helpers/pdf/PdfExporterTest.java
+++ src/test/java/ch/zhaw/roma/helpers/pdf/PdfExporterTest.java
@@ -1,22 +1,30 @@
-package ch.zhaw.roma.helpers.pdf.test;
+package ch.zhaw.roma.helpers.pdf;
 
-import ch.zhaw.roma.helpers.pdf.PdfExportData;
-import ch.zhaw.roma.helpers.pdf.PdfExporter;
 import ch.zhaw.roma.model.royaltycalculation.AccountEntry;
 import ch.zhaw.roma.model.royaltycalculation.AuxRight;
 import ch.zhaw.roma.model.royaltycalculation.LegacyEntry;
+import org.junit.Before;
+import org.junit.Test;
 
+
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 
-public class PdfTest {
+import static org.junit.Assert.*;
 
-    public static void main(String[] args) {
-        PdfExportData data = new PdfExportData();
 
+
+public class PdfExporterTest {
+
+    private PdfExportData data;
+
+    @Before
+    public void before() {
+
+        data = new PdfExportData();
         data.setPlaceAndDate("Zürich, 20.Januar 2019");
-
         data.setGreeting("Herr");
         data.setFirstName("Paul");
         data.setLastName("Pedeef");
@@ -24,11 +32,9 @@ public class PdfTest {
         data.setZipCode("8001");
         data.setCity("Zürich");
         data.setCountry("Schweiz");
-
-        data.setISBN("978-3-86894-9117");
+        data.setISBN("978-3-86894-9118");
         data.setTitle("Meine grosse Liebe PDF");
         data.setAuthor("Paul Pedeef");
-
         data.setPriceHardCoverChf(29.55);
         data.setPriceHardCoverChfExMwSt(26.45);
         data.setPriceHardCoverEur(21.65);
@@ -36,10 +42,8 @@ public class PdfTest {
         data.setPriceEbookEur(14.40);
         data.setPriceEbookEurExMwSt(9.95);
         data.setPercentageNetRevEbook(25);
-
         data.setPeriodStart("1.1.2018");
         data.setPeriodEnd("31.12.2018");
-
         LegacyEntry l1 = new LegacyEntry(2015, 69);
         LegacyEntry l2 = new LegacyEntry(2016, 1700);
         LegacyEntry l3 = new LegacyEntry(2017, 5059);
@@ -48,21 +52,17 @@ public class PdfTest {
         legacy.add(l2);
         legacy.add(l3);
         data.setLegacyEntries(legacy);
-
         data.setSoldCh(139);
         data.setSoldDeAu(598);
         data.setSoldEbooks(1090);
-
         data.setHonoraryRate(7);
         data.setHonoraryPerCopyCH(2.27);
         data.setHonoraryPerCopyDeAu(1.89);
-
         data.setSumChChf(453.80);
         data.setSumChEur(420.74);
         data.setSumDeAuEur(4021.34);
         data.setSumEbooksEur(598.32);
         data.setNetRevEbooks(230.87);
-
         ArrayList<AuxRight> auxL = new ArrayList<>();
         AuxRight auxR = new AuxRight();
         auxR.setTitle("Hörbuch 2018");
@@ -74,7 +74,7 @@ public class PdfTest {
         data.setAuxRights(auxL);
 
         double auxTotal = 0;
-        for(AuxRight auxRe : auxL) {
+        for (AuxRight auxRe : auxL) {
             auxTotal += auxRe.getSumEur();
         }
         data.setAuxRightsTotal(auxTotal);
@@ -83,25 +83,31 @@ public class PdfTest {
         ArrayList<AccountEntry> entries = new ArrayList<>();
         entries.add(accountE);
         data.setEntries(entries);
-
         data.setRoyalityCalculationTotal(data.getSumChEur() + data.getSumDeAuEur() + data.getSumEbooksEur() + data.getAuxRightsTotal());
 
         double accountEntryTotal = 0;
-        for(AccountEntry entry : entries) {
+        for (AccountEntry entry : entries) {
             accountEntryTotal += entry.getAmount();
         }
         data.setAccountTotal(accountEntryTotal + data.getRoyalityCalculationTotal());
-
         data.setContactDetails("Tel +41 44 444 44 44\nFax +41 44 999 99 99\nwww.mb-verlag.ch\nverlag@mb-verlag.ch");
+        data.setExRateEurChf(1.13);
+    }
 
+    @Test
+    public void printTest() {
+        String path = "output/TestPDF.pdf";
         try {
-            PdfExporter exporter = new PdfExporter("output/TestPDF.pdf", data);
-
+            PdfExporter exporter = new PdfExporter(path, data);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        //File file = new File(this.getClass().getClassLoader().getResource(path).getFile());
+        File file = new File(path);
 
+        assertTrue(file.exists());
+        assertTrue(file.getTotalSpace() > 15000);
     }
 }
